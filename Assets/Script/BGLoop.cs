a97b9f5016b6c265299d5dbd7cf9a2a252ea2d7c
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-06-17 13:38:59 +0900

    各種UIの調整、背景のループが未完成だったため修正

--- Assets/Script/BGLoop.cs
+++ Assets/Script/BGLoop.cs
@@ -41,17 +41,57 @@ public class BGLoop : MonoBehaviour
                     a.transform.position -= move;
                 }
             }
+
             if (player.transform.position.x <= minX.transform.position.x)//プレイヤーがマップ左端に到達時
             {
                 player.transform.position = new Vector2(right.position.x, player.transform.position.y);//マップ右側の左端にワープ
+                Vector3 move = new Vector3(minX.position.x - right.position.x, 0, 0);
+                foreach (GameObject a in enemy)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in exp)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in bullet)
+                {
+                    a.transform.position -= move;
+                }
             }
             if (player.transform.position.y >= maxY.transform.position.y)//プレイヤーがマップ上端に到達時
             {
                 player.transform.position = new Vector2(player.transform.position.x, lower.position.y);//マップ下側の上端にワープ
+                Vector3 move = new Vector3(0,maxY.position.y - lower.position.y, 0);
+                foreach (GameObject a in enemy)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in exp)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in bullet)
+                {
+                    a.transform.position -= move;
+                }
             }
             if (player.transform.position.y <= minY.transform.position.y)//プレイヤーがマップ下端に到達時
             {
                 player.transform.position = new Vector2(player.transform.position.x, upper.position.y);//マップ上側の下端にワープ
+                Vector3 move = new Vector3(0, minY.position.y - upper.position.y, 0);
+                foreach (GameObject a in enemy)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in exp)
+                {
+                    a.transform.position -= move;
+                }
+                foreach (GameObject a in bullet)
+                {
+                    a.transform.position -= move;
+                }
             }
         }
     }
